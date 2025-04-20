9b03689ca22aa1c283f17835e312b5fc061ab7a1
adriancancode <vazqueza2004@gmail.com>
2025-04-20 10:14:28 -0700

    Create Bash file to run servers

--- frontend/convex/auth.config.ts
+++ frontend/convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://pleasing-finch-3.clerk.accounts.dev",
+      domain: "https://gentle-flounder-43.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
