25f1fda338b8fe3a50835c98f089eca18399cdee
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-20 04:16:11 -0700

    spotlight effect!

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,15 @@
 
 import Navbar from "@/components/navbar";
 import Header from "@/components/header";
+import SpotlightCard from "@/components/spotlight";
 
 export default function Home() {
   return (
     <div className="min-h-full flex flex-col">
-      <Navbar />
-      <Header />
+      <SpotlightCard>
+        <Navbar />
+        <Header />
+      </SpotlightCard>
     </div>
   );
 }
